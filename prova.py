fcc3c251b4e11e7a3e5dfe5ce8ad1099185f0e84
avrm000 <criscarrieri13@gmail.com>
2024-09-08 18:59:15 +0200

    aggiunto dizionario json

--- prova.py
+++ prova.py
@@ -2,10 +2,18 @@ import streamlit as st
 import csv
 import json
 
+
+dict = {}
+
 with open('words2.csv', 'r', encoding = 'utf-8') as file: 
 
     reader = csv.reader(file)
 
     for riga in reader: 
-        print(riga) 
+        dict[riga[1]] = riga[0]
+
+with open('words.json', 'w', newline = '', encoding = 'utf-8') as file: 
+
+    json.dump(dict, file, ensure_ascii=False, indent = 4)
+
 
